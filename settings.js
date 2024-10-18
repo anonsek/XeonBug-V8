bc4096e96e761da54890d8c944b78eb666eab887
M SAQLAIN <157575580+anonsek@users.noreply.github.com>
2024-10-18 21:22:19 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
-global.ytname = "YT: GlobalTechInfo"
+global.ownernomer = "92470019309"
+global.ownername = "Sakki Here"
+global.ytname = "Telegram : sakki_47"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '923470019309'  //creator number
+global.ownername = 'Sakki' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
